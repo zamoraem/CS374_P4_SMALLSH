63d8b8631ebba69897e5c1f651056710ec7666b5
zamoraem <56068057+zamoraem@users.noreply.github.com>
2026-05-15 16:52:32 -1000

    Create zamorael_assignment4.c

---
+++ zamorael_assignment4.c
@@ -0,0 +1,63 @@
+/**
+ * A sample program for parsing a command line. If you find it useful,
+ * feel free to adapt this code for Assignment 4.
+ * Do fix memory leaks and any additional issues you find.
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT_LENGTH 2048
+#define MAX_ARGS		 512
+
+
+struct command_line
+{
+	char *argv[MAX_ARGS + 1];
+	int argc;
+	char *input_file;
+	char *output_file;
+	bool is_bg;
+};
+
+
+struct command_line *parse_input()
+{
+	char input[INPUT_LENGTH];
+	struct command_line *curr_command = (struct command_line *) calloc(1, sizeof(struct command_line));
+
+	// Get input
+	printf(": ");
+	fflush(stdout);
+	fgets(input, INPUT_LENGTH, stdin);
+
+	// Tokenize the input
+	char *token = strtok(input, " \n");
+	while(token){
+		if(!strcmp(token,"<")){
+			curr_command->input_file = strdup(strtok(NULL," \n"));
+		} else if(!strcmp(token,">")){
+			curr_command->output_file = strdup(strtok(NULL," \n"));
+		} else if(!strcmp(token,"&")){
+			curr_command->is_bg = true;
+		} else{
+			curr_command->argv[curr_command->argc++] = strdup(token);
+		}
+		token=strtok(NULL," \n");
+	}
+	return curr_command;
+}
+
+int main()
+{
+	struct command_line *curr_command;
+
+	while(true)
+	{
+		curr_command = parse_input();
+
+	}
+	return EXIT_SUCCESS;
+}
